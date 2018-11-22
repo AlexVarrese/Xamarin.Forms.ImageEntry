a4ed6055a056ad56cedbd1bc439afb71f0a44379
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-11-22 14:11:09 -0200

    :sparkles: New Code Add

---
+++ Xamarin.Forms.ImageEntry/ImageEntryRenderer.android.cs
@@ -0,0 +1,67 @@
+﻿using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Support.V4.Content;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.ImageEntry;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(ImageEntry), typeof(ImageEntryRenderer))]
+namespace Xamarin.Forms.ImageEntry
+{
+    public class ImageEntryRenderer : EntryRenderer
+    {
+        /// <summary>
+        /// Used for registration with dependency service
+        /// </summary>
+        public async static void Init()
+        {
+            var temp = DateTime.Now;
+        }
+
+        ImageEntry element;
+        public ImageEntryRenderer(Context context) : base(context)
+        {
+
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || e.NewElement == null)
+                return;
+
+            element = (ImageEntry)this.Element;
+
+
+            var editText = this.Control;
+            if (!string.IsNullOrEmpty(element.Image))
+            {
+                switch (element.ImageAlignment)
+                {
+                    case ImageAlignment.Left:
+                        editText.SetCompoundDrawablesWithIntrinsicBounds(GetDrawable(element.Image), null, null, null);
+                        break;
+                    case ImageAlignment.Right:
+                        editText.SetCompoundDrawablesWithIntrinsicBounds(null, null, GetDrawable(element.Image), null);
+                        break;
+                }
+            }
+            editText.CompoundDrawablePadding = 25;
+            Control.Background.SetColorFilter(element.LineColor.ToAndroid(), PorterDuff.Mode.SrcAtop);
+        }
+
+        private BitmapDrawable GetDrawable(string imageEntryImage)
+        {
+            int resID = Resources.GetIdentifier(imageEntryImage, "drawable", this.Context.PackageName);
+            var drawable = ContextCompat.GetDrawable(this.Context, resID);
+            var bitmap = ((BitmapDrawable)drawable).Bitmap;
+
+            return new BitmapDrawable(Resources, Bitmap.CreateScaledBitmap(bitmap, element.ImageWidth * 2, element.ImageHeight * 2, true));
+        }
+
+    }
+}
