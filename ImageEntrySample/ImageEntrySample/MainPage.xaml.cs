aa4116efc6f336a411ac3c21fc94b442d3552c55
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2019-01-03 14:14:43 -0200

    :sparkles: add sample

---
+++ ImageEntrySample/ImageEntrySample/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ImageEntrySample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Xamarin.Forms.ImageEntry.ImageEntry teste = new Xamarin.Forms.ImageEntry.ImageEntry();
+        }
+    }
+}
